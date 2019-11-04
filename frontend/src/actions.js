d64234bd92a68bf673fb75c12f80387d43746157
Kathy Nguyen <kathyn262@gmail.com>
2019-11-03 20:49:39 -0800

    ready to reploy frontend

--- frontend/src/actions.js
+++ frontend/src/actions.js
@@ -10,7 +10,7 @@ import {
   VOTE
 } from "./actionTypes";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
 
 function getTitles(titles) {
   return {
