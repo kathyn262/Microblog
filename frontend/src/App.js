23b4a6ad11523a6bd779a50aac6bb3d45aad97be
Kathy Nguyen <kathyn262@gmail.com>
2020-10-14 17:54:45 -0700

    added backend ping on app launch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 import NavBox from './Components/NavBox';
 import Routes from './Components/Routes';
 
+const BASE_URL = "http://localhost:5000";
 class App extends React.Component {
+
+  async componentDidMount() {
+    let wake = await axios.get(`${BASE_URL}/ping`);
+    console.log(wake.data);
+  }
   render() {
     return (
       <div className="container">
